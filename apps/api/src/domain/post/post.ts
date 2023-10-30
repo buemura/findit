74fe50ced1fc079c3a7784950001823e012bf832
Bruno <bruno.uemura1@gmail.com>
2023-10-30 13:16:44 -0300

    refactor: adds initial refactoring

---
+++ apps/api/src/domain/post/post.ts
@@ -0,0 +1,14 @@
+export class Post {
+  id: string;
+  userId: string;
+  categoryId: string;
+  title: string;
+  description: string;
+  price: number;
+  city: string;
+  state: string;
+  country: string;
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
